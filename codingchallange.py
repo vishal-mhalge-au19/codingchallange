e1db64ff314e20e024ad31bbd7376e48fea8ae50
vishal-mhalge-au19 <85360494+Vishalmhalge@users.noreply.github.com>
2021-06-10 00:02:29 +0530

    codingchallange.py
     asignment

---
+++ codingchallange.py
@@ -0,0 +1,8 @@
+name = Vishal Mhalge
+print ("my name is",name,":)")
+age = 24
+print("my age is", age,":)")
+number = 7757827515
+print ("my mobile number is", number,")")
+location = Pune
+print("my location is", location,")")
